aa329e89423d7cfa65e29de720bafc4708c421d8
lolhaha95 <bryan_wang19@hotmail.com>
2018-10-02 17:08:56 +0800

    first

---
+++ createProject.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Create Project</title>
+  <style>li {list-style: none;}</style>
+    </head>
+    <body>
+
+  <h1>Create your Project!</h1>
+  <h2>Please fill in the Relevant details below</h2>
+    </body> 
+    
+<?php
+		//getting variable from another php
+		session_start();
+        include('dbconnect.php');
+        
+?>
+
+</head>
+</html>
