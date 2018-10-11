378e40828a60d1db9e2ca22bddd07065c7ffd778
wufeiyu <37360468+noflyingfish@users.noreply.github.com>
2018-10-11 15:19:54 +0800

    git test

--- search.php
+++ search.php
@@ -29,60 +29,46 @@
             $check = false;
         }
 
-
         if($check){
             $sql = "SELECT title, description, start_date, end_date, curr$, total$ FROM project WHERE title LIKE '%$q%'";
-            //echo "$sql <br/>"; // for debugging
-
     	}
     	else{
             $sql = "SELECT title, description, start_date, end_date, curr$, total$ FROM project";
-            //echo "$sql <br/>"; // for debugging
             $q = "everything";
     	}
 
     	$result = pg_query($db, $sql);
-
     	//here on prints the seach results nicely in a table
         $i = 0;
         echo "<html><body><table><tr>";
-        while ($i < pg_num_fields($result))
-        {
+        while ($i < pg_num_fields($result)){
             $fieldName = pg_field_name($result, $i);
-            echo "<td>  " . $fieldName . " </td>";
+            echo "<td>  " . $fieldName . " </td> ";
             $i = $i + 1;
         }
         echo "</tr>";
         $i = 0;
 
-        while ($row = pg_fetch_row($result))
-        {
+        while ($row = pg_fetch_row($result)){
             echo "<tr>";
             $count = count($row);
             $y = 0;
-            while ($y < $count)
-            {
-
+            while ($y < $count) {
                 $c_row = current($row);
 
-
-                   if ($y % 4 == 0 && $y != 0){
-                      echo "<td> " . $c_row . " &nbsp <button>Support</button> </td>";
-
+                if ($y % 4 == 0 && $y != 0){
+                    echo "<td> " . $c_row . " &nbsp <button>Support</button> </td>";
                 }else{
-                       echo "<td> " . $c_row . " &nbsp </td>";
+                    echo "<td> " . $c_row . " &nbsp </td>";
                 }
-
 
                 next($row);
                 $y = $y + 1;
             }
             echo "</tr>";
-            $i = $i + 1;
         }
         pg_free_result($result);
         echo "</table></body></html>";
-
 
     	if($result) echo "Search results for $q returned <br/>";
         $check = true;
