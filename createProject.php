2eb0684a0cd7ebbf5e3870cce84878f5b703f8d0
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-10-16 22:22:08 +0800

    fix create project 1. sql function insert project into project table 2. sql function insert keyword into keywords table

--- createProject.php
+++ createProject.php
@@ -40,55 +40,66 @@
 <?php
 		//getting variable from another php
 		session_start();
-        include('dbconnect.php');
-        $user_email = $_SESSION["user_email"];
+    include('dbconnect.php');
+    $user_email = $_SESSION["user_email"];
 
-        if(isset($_POST['submit'])){
-          $title = $_POST['title'];
-          $description = $_POST['description'];
-          $startDate = $_POST['startDate'];
-          $endDate = $_POST['endDate'];
-          $target = $_POST['targetCapital'];
-          $curr = $_POST['currentCapital'];
-          $keywords = $_POST['keywords'];
-          $create = true;
-
-
-          //Validation for blank info
-          if ($title == "" || $startDate == "" || $endDate == "" || $target == "" || $curr == "" || $keywords == "" || $description == "") {
-            $create = false;
-            echo "Please ensure all details are filled";
-          }
-
-          //Add to database
-          //Task1: Please note that if admin attempts to create a project, an error will be raised because the 'own' column references users(email) in which the admin's email is not found. Multiple workarounds are possible. 
-          //Task2: Also need to find a way to insert the many different keywords into the keywords table.  
-          if($create) {
-            $sql_project = "INSERT INTO project (curr$, total$, title, description, project_keywords, start_date, end_date, own) VALUES('".$curr."', '".$target."', '".$title."', '".$description."', '".$keywords."', '".$startDate."', '".$endDate."', '".$user_email."') RETURNING id";
-            $add_project = pg_query($db,$sql_project);
-            $result = pg_fetch_row($add_project);
-            $last_id = $result[0];
-    
+    if(isset($_POST['submit'])){
+      $title = $_POST['title'];
+      $description = $_POST['description'];
+      $startDate = $_POST['startDate'];
+      $endDate = $_POST['endDate'];
+      $target = $_POST['targetCapital'];
+      $curr = $_POST['currentCapital'];
+      $keywords = $_POST['keywords'];
+      $myArray = explode(',', $keywords);
+      //print_r($myArray);
+      $create = true;
 
 
-            if($add_project){
-                echo "Project $title has been successfully created";
-            }else {
-                echo "failed";    //debug
+      //Validation for blank info
+      if ($title == "" || $startDate == "" || $endDate == "" || $target == "" || $curr == "" || $keywords == "" || $description == "") {
+        $create = false;
+        echo "Please ensure all details are filled";
+      }
+
+      //Add to database
+      //Task1: Please note that if admin attempts to create a project, an error will be raised because the 'own' column references users(email) in which the admin's email is not found. Multiple workarounds are possible.
+      //Task2: Also need to find a way to insert the many different keywords into the keywords table.
+      if($create) {
+        $sql_project = "INSERT INTO project (curr$, total$, title, description, start_date, end_date, own) VALUES('".$curr."', '".$target."', '".$title."', '".$description."', '".$startDate."', '".$endDate."', '".$user_email."') RETURNING id";
+        $add_project = pg_query($db,$sql_project);
+        $result = pg_fetch_row($add_project);
+        $last_id = $result[0];
+        $add_keyword;
+        if($add_project){
+          echo "Project $title has been successfully created";
+          foreach($myArray as $tag) {
+            if ((preg_match("/^[a-zA-Z0-9]+$/", $tag) == 1)) {
+              $sql_keyword = "INSERT INTO keywords (id, word) VALUES ('".$last_id."', '".$tag."')";
+              $add_keyword = pg_query($db,$sql_keyword);
             }
-          }else{
-                echo "failed2";   //debug
           }
-          $create = true; //to reset
+        }
+        else {
+          echo "Project $title was not added";
         }
 
-         if(isset($_POST['back_btn'] )){
-         
-            if($_SESSION["mod"] == false){
-                header("Location: profile.php");
-                }else {
-                     header("Location: admin.php");
-                }
+        if($add_keyword) {
+            echo "<br>Keywords recorded to project";
+        }else {
+            echo "<br>Failed to add keywords";    //debug
+        }
+      }
+      $create = true; //to reset
+    }
+
+     if(isset($_POST['back_btn'] )){
+
+      if($_SESSION["mod"] == false){
+          header("Location: profile.php");
+      }else {
+          header("Location: admin.php");
+      }
     }
 ?>
 
