0856d87f9e524a76ffc069148db3add5a5fb8f73
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-10-20 12:23:11 +0800

    add on for searching via keywords in search.php fix "view details button"

--- search.php
+++ search.php
@@ -21,7 +21,7 @@
 <?php
     include('dbconnect.php');
     session_start();
-
+    $c = 1;
     if(isset($_POST['search_btn'])){
     	$q = $_POST['query'];
         $check = true;
@@ -31,51 +31,60 @@
         }
 
         if($check){
-            $sql = "SELECT id, title, description, start_date, end_date, curr$, total$ FROM project WHERE title LIKE '%$q%'";
+            $sql = //"SELECT p.id, p.title, p.description, p.start_date, p.end_date, p.curr$, p.total$
+										//FROM project p WHERE UPPER(p.title) LIKE UPPER('%$q%')";
+										"SELECT DISTINCT p.id, p.title, p.description, p.start_date, p.end_date, p.curr$, p.total$
+										FROM project p, keywords k
+										WHERE k.id = p.id
+										AND ((UPPER(p.title) LIKE UPPER('%$q%')) OR (UPPER(k.word) LIKE UPPER('%$q%')))";
     	}else{
             $sql = "SELECT id, title, description, start_date, end_date, curr$, total$ FROM project";
             $q = "everything";
     	}
 
     	$result = pg_query($db, $sql); // the result of the query
-        $col = pg_num_fields($result); // the number of column
+      $col = pg_num_fields($result); // the number of column
 
-        echo '<table border = "1"><tr>'; //html code for the table
-        for($x = 1; $x < $col; $x++) { //for loop to print the table headings, starts at 1 to skip id
-            $fieldName = pg_field_name($result, $x); //$result as an array, $x as index
-            echo "<th>".$fieldName."</th>";
-        }
-        echo "<th>Sarpork!</th>";
-        
-        while ($row = pg_fetch_row($result)){ //$row is an array of each row of data
-            echo "<tr>"; //html for new row, for data
-            for($y = 1; $y < $col; $y++){ // starts at 1 to skip id
-                echo "<td> $row[$y] </td>";
-            };
-            echo "<td> <form name=\"support_btn\" method=\"POST\">
-                    <button type=\"support\" name=\"support\"> SUPPORT
-                    <button type=\"details\" name=\"details\" value=\" $row[0]\"> View Details";
-                        
-                    //admin.php will route here to use search.php to find projects to modify. This checks if the session requesting this page is mod-enabled or not and if so, //returns an additional button for moderators to use
-                    if($_SESSION["mod"] == true){           
-                        echo "<button type=\"modify\" name=\"modify\" value=\" $row[0]\"> Modify";
-                    }
-                        echo "</form>";
-            // support row, hidden by default. // need to make this appear on click on support button
-            echo "<tr name=\"support_row\" style =\"display:none\">
-                    <td>Amount to support:</td>
-                    <td><input type=\"text\" placeholder =\"Supporting Amount\" name=\"amt_support\"></td> 
-                    <td><input type = \"submit\" value =\"Support Your Amount!\" name=\"btn_support\"></td>";
-        }
-        echo "</table>";
+			//print table headers
+      echo '<table border = "1"><tr>'; //html code for the table
+      for($x = 1; $x < $col; $x++) { //for loop to print the table headings, starts at 1 to skip id
+          $fieldName = pg_field_name($result, $x); //$result as an array, $x as index
+          echo "<th>".$fieldName."</th>";
+      }
+      echo "<th>Sarpork!</th>";
 
-    	if($result) echo "Search results for $q returned <br/>";
-        $check = true;
+      $c = 1; // c as a counter to loop through, and index of each row
+      while ($row = pg_fetch_row($result)){ //$row is an array of each row of data
+
+        echo "<tr>"; //html for new row, for data
+        for($y = 1; $y < $col; $y++){ // starts at 1 to skip id
+            echo "<td> $row[$y] </td>";
+        };
+        echo "<td> <button type=\"submit\" name=\"support\" onclick=\"show($c)\" > SUPPORT
+								<form name=\"support_btn\" method=\"POST\">
+                <button type=\"submit\" name=\"details\" value=\" $row[0]\"> View Details
+							</form>";
+
+        //admin.php will route here to use search.php to find projects to modify. This checks if the session requesting this page is mod-enabled or not and if so, //returns an additional button for moderators to use
+        if($_SESSION["mod"] == true){
+            echo "<button type=\"modify\" name=\"modify\" value=\" $row[0]\"> Modify";
+        }
+
+        // support row, hidden by default. // need to make this appear on click on support button
+        echo "<tr name=\"support_row\" style =\"display:none\" id=\"support_row_$c\">
+                <td>Amount to support:</td>
+                <td><input type=\"text\" placeholder =\"Supporting Amount\" name=\"amt_support_$c\"></td>
+                <td><input type = \"submit\" value =\"Support Your Amount!\" name=\"btn_support_$c\"  ></td>";
+
+        $c = $c +1;
+      }
+      echo "</table>";
+
+	  	if($result) echo "Search results for $q returned <br/>";
+	      $check = true;
     }
 
-    if(isset($_POST['support'])){
-        header("Location: placeholder.php");
-    }
+
     if(isset($_POST['details'])){
         $_SESSION["id"] = $_POST['details'];
         header("Location: item.php");
@@ -89,13 +98,29 @@
         header("Location: profile.php");
         }
     }
-    
+
     //for admin use only
      if(isset($_POST['modify'])){
         $_SESSION["id"] = $_POST['modify'];//pass id into moderate page. moderate.php will now directly pull up the project with id $_SESSION["id"] for the admin to edit
         header("Location: moderate.php");
     }
 ?>
+
+    <script>
+        function show(c){
+            //alert(c);
+            var displaySetting = document.getElementById('support_row_'+c).style.display;
+            var xx = document.getElementById('support_row_'+c);
+            //alert(displaySetting);
+            //alert(xx);
+            if(displaySetting == 'none'){
+                xx.style.display = 'block';
+            }else{
+                xx.style.display = 'none';
+            }
+
+        }
+    </script>
 
 </body>
 </html>
